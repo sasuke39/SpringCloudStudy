03713b28caf65c68907ac04917c0a2a79984d604
xianhong.zhou <xianhong.zhou@dtminds.com>
2022-06-13 09:36:14 +0800

    add

--- middleware/gateway/src/main/java/com/geekbang/gateway/RedisLimitationConfig.java
+++ middleware/gateway/src/main/java/com/geekbang/gateway/RedisLimitationConfig.java
@@ -1,6 +1,10 @@
 package com.geekbang.gateway;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.cloud.gateway.filter.ratelimit.RateLimiter;
 import org.springframework.cloud.gateway.filter.ratelimit.RedisRateLimiter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,35 +17,40 @@ import reactor.core.publisher.Mono;
  */
 
 @Configuration
+@Slf4j
 public class RedisLimitationConfig {
 
     // 限流的维度
     @Bean
     @Primary
     public KeyResolver remoteHostLimitationKey() {
-        return exchange -> Mono.just(
-                exchange.getRequest()
-                        .getRemoteAddress()
-                        .getAddress()
-                        .getHostAddress()
-        );
+        return exchange -> {
+            String hostAddress = exchange.getRequest()
+                    .getRemoteAddress()
+                    .getAddress()
+                    .getHostAddress();
+            log.info("limit path:{}",hostAddress);
+            return Mono.just(
+                    hostAddress
+            );
+        };
     }
 
     //template服务限流规则
     @Bean("tempalteRateLimiter")
     public RedisRateLimiter templateRateLimiter() {
-        return new RedisRateLimiter(10, 20);
+        return new RedisRateLimiter(1, 1);
     }
 
     // customer服务限流规则
     @Bean("customerRateLimiter")
     public RedisRateLimiter customerRateLimiter() {
-        return new RedisRateLimiter(1, 1);
+        return new RedisRateLimiter(0, 1);
     }
 
     @Bean("defaultRateLimiter")
     @Primary
     public RedisRateLimiter defaultRateLimiter() {
-        return new RedisRateLimiter(50, 100);
+        return new RedisRateLimiter(1, 1);
     }
 }
